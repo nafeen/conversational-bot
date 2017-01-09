ba4f56a45a2c3e303c3d5a53a2027e8a8da1490a
Abdul Nafeen <abdulnafeen@yahoo.com>
2017-01-09 12:25:28 +0530

    SET-UP BASIC UI/UX ELEMENTS
    - ADDED a Contact Card to give the bot, a human touch
    - ADDED a sample conversational data file in JSON format
    - CREATED a first version of the UI using elements from Bootstrap and Font Awesome
    - WROTE an initial programmatic rendering of conversation. The logic is incorrect
    - MODIFIED .gitignore to add APPS/

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
+/**********************************************************************************************
+ *
+ * THIS SCRIPT CREATES SERVER AND HANDLES ROUTING
+ * ----------------------------------------------
+ * 								Author | Nafeen
+ *
+ ********************************************************************************************** */
+
+
+/* ============================================================================================
+ *
+ * TO-DO
+ * -----
+ * 1 -	Handle errors conditions
+ *
+/* ============================================================================================ */
+
+
+// ADDING middlewares and other dependencies
 var express            =   require('express');
 var app                =   express();
 
-app.get('/', function (req, res) {
-  res.send('Converse with me...')
-})
+
+app.use('/public', express.static('./apps/default/public'));
+
+// ROUTING for root path 
+app.get('/*', require('./apps/default/route.js'))
+
+
+// ROUTING for views 
+app.set('views', [__dirname + '/apps/default/views']);
+
+
+/* ============================================================================================ */
+/* CREATES Server listening on port 3000 and logs message on console */
+/* ============================================================================================ */
 
 app.listen(3000, function () {
   console.log('Conversational App listening on port 3000!')
-})
+});
+
+/*********************************************************************************************/
+/*********************************************************************************************/
